34dcf06f28d7b73df934853183c705e23cafd342
Yuri Castro Coelho <yuricastrocoelho90@gmail.com>
2026-06-19 17:17:41 -0300

    Corrige erros

--- backend-domus/app/main.py
+++ backend-domus/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",  # React local
+        "https://domus-laguna.vercel.app/",  # React local
     ],
     allow_credentials=True,
     allow_methods=["*"],

7d923d8e0e0cda08a46e17e742972161a24c9aa2
João Marcelo <joobrecha@gmail.com>
2026-06-18 22:56:43 -0300

    Add Favorito model and update Imovel model

--- backend-domus/app/models.py
+++ backend-domus/app/models.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from app.database import Base
+
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    senha = Column(String, nullable=False)
+
 
 class Imovel(Base):
     __tablename__ = "imoveis"
 
-    __table_args__ = {'extend_existing': True}
-
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     tipo = Column(String)
     bairro = Column(String)
     endereco = Column(String)
@@ -16,3 +24,11 @@ class Imovel(Base):
     longitude = Column(Float)
     imagem = Column(String)
     contato = Column(String)
+
+
+class Favorito(Base):
+    __tablename__ = "favoritos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+    imovel_id = Column(Integer, ForeignKey("imoveis.id"))

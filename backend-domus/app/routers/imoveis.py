d314a831411083a287dce9bef5b1ded1f8500c9a
João Marcelo <joobrecha@gmail.com>
2026-06-18 22:59:50 -0300

    Refactor imoveis router to use CRUD functions

--- backend-domus/app/routers/imoveis.py
+++ backend-domus/app/routers/imoveis.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from typing import List
-
 
 from app.database import SessionLocal
-from app import models, schemas
+from app import schemas, crud
 
-router = APIRouter(prefix="/imoveis", tags=["Imóveis"])
+router = APIRouter(
+    prefix="/imoveis",
+    tags=["Imoveis"]
+)
 
 
 def get_db():
@@ -16,6 +17,28 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/", response_model=List[schemas.Imovel])
-def listar_imoveis(db: Session = Depends(get_db)):
-    return db.query(models.Imovel).all()
+
+@router.get("/")
+def listar(db: Session = Depends(get_db)):
+    return crud.listar_imoveis(db)
+
+
+@router.get("/{id}")
+def buscar(id: int, db: Session = Depends(get_db)):
+    return crud.buscar_imovel(db, id)
+
+
+@router.post("/")
+def criar(
+    imovel: schemas.ImovelCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.criar_imovel(db, imovel)
+
+
+@router.delete("/{id}")
+def deletar(
+    id: int,
+    db: Session = Depends(get_db)
+):
+    return crud.deletar_imovel(db, id)
